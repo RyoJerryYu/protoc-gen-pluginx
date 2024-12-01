347a634e9f2a7d1bf32baf87a6708fb8ee0399c8
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-12-01 20:30:23 +0800

    feat: move proto package to real name

--- tests/protoc-gen-go-fieldmask/e2e/e2e.go
+++ tests/protoc-gen-go-fieldmask/e2e/e2e.go
@@ -2,6 +2,6 @@ package e2e
 
 //go:generate protoc --go_out=. --go_opt=paths=source_relative proto/user/user.proto proto/feed/feed.proto
 //go:generate go build -o ./protoc-gen-go-fieldmask ../../../cmd/protoc-gen-go-fieldmask
-//go:generate protoc --plugin=protoc-gen-go-fieldmask=./protoc-gen-go-fieldmask --go_out=. --go_opt=paths=source_relative --go-fieldmask_out=. --go-fieldmask_opt=paths=source_relative,logtostderr=true,v=1 proto/user/user.proto
 //go:generate protoc --plugin=protoc-gen-go-fieldmask=./protoc-gen-go-fieldmask --go_out=. --go_opt=paths=source_relative --go-fieldmask_out=. --go-fieldmask_opt=paths=source_relative,logtostderr=true,v=1 proto/user/user_password.proto
+//go:generate protoc --plugin=protoc-gen-go-fieldmask=./protoc-gen-go-fieldmask --go_out=. --go_opt=paths=source_relative --go-fieldmask_out=. --go-fieldmask_opt=paths=source_relative,logtostderr=true,v=1 proto/user/user.proto
 //go:generate protoc --plugin=protoc-gen-go-fieldmask=./protoc-gen-go-fieldmask --go_out=. --go_opt=paths=source_relative --go-fieldmask_out=. --go-fieldmask_opt=paths=source_relative,logtostderr=true,v=1 proto/feed/feed.proto
