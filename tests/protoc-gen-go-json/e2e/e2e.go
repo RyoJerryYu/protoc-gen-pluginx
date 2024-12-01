845d6677b9f1ac01fa11a4c837a7cfdf7adc7963
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-12-01 21:06:03 +0800

    feat: add buf for json

--- tests/protoc-gen-go-json/e2e/e2e.go
+++ tests/protoc-gen-go-json/e2e/e2e.go
@@ -1,6 +1,4 @@
 package e2e
 
 //go:generate go build -o ./protoc-gen-go-json ../../../cmd/protoc-gen-go-json
-//go:generate protoc --plugin=protoc-gen-go-json=./protoc-gen-go-json --go_out=. --go_opt=paths=source_relative --go-json_out=. --go-json_opt=paths=source_relative e2e.proto
-//go:generate protoc --plugin=protoc-gen-go-json=./protoc-gen-go-json --go_out=. --go_opt=paths=source_relative --go-json_out=. --go-json_opt=paths=source_relative e2e_service.proto
-//go:generate protoc --plugin=protoc-gen-go-json=./protoc-gen-go-json --go_out=. --go_opt=paths=source_relative --go-json_out=. --go-json_opt=paths=source_relative e2e_enum.proto
+//go:generate buf generate
