d2fcbe58207da2b8dc092781b95bac5421b7fd34
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-12-01 21:04:52 +0800

    feat: update go genearaet

--- tests/protoc-gen-go-fieldmask/well_known/well_known.go
+++ tests/protoc-gen-go-fieldmask/well_known/well_known.go
@@ -1,4 +1,4 @@
 package well_known
 
 //go:generate go build -o ./protoc-gen-go-fieldmask ../../../cmd/protoc-gen-go-fieldmask
-//go:generate protoc --plugin=protoc-gen-go-fieldmask=./protoc-gen-go-fieldmask --go_out=. --go_opt=paths=source_relative --go-fieldmask_out=. --go-fieldmask_opt=paths=source_relative well_known.proto
+//go:generate buf generate
